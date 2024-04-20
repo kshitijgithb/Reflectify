e5598c5fabfe54da92313865a22d957887f0d46c
kshitijgithb <kshitijkumargupta2017@gmail.com>
2024-04-20 20:51:56 +0530

    Added Base URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
 
+const API = axios.create( `${REACT_APP_API}` );
+// const API = `${REACT_APP_API}`;
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
